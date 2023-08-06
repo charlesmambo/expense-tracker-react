196a14379212754b473f69080ac5f01f9aef50d1
Charles Mambo <chylah11st@gmail.com>
2023-08-06 11:03:08 +0200

    Complete project layout step1

---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export const IncomeExpenses = () => {
+  return (
+    <div class='inc-exp-conatiner'>
+        <div>
+            <h4>Income</h4>
+            <p id='money-plus' className='money plus'>+$0.00</p>
+        </div>
+
+        <div>
+            <h4>Expenses</h4>
+            <p id='money-minus' className='money minus'>-$0.00</p>
+        </div>
+
+    </div>
+  )
+}
